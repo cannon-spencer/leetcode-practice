245e3823e0beb889b44c029eefe13fad5bb89ce5
Cannon Spencer <102569627+cannon-spencer@users.noreply.github.com>
2025-04-10 16:11:10 -0400

    remove element

---
+++ remove_element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+        int removeElement(vector<int>& nums, int val) {
+            int notRemoved = 0;
+    
+            for(int i = 0; i < nums.size(); i++){
+                if(nums[i] != val){
+                    nums[notRemoved++] = nums[i];
+                }
+            }
+    
+            return notRemoved;
+        }
+    };
