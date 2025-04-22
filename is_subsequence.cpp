a3254e18b863802079702efb7330f8499737dfc1
Cannon Spencer <102569627+cannon-spencer@users.noreply.github.com>
2025-04-22 13:01:31 -0400

    subsequence

---
+++ is_subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        bool isSubsequence(string s, string t) {
+            
+            if(s.empty()) return true;
+    
+            int j = 0;
+    
+            for(int i = 0; i < t.size(); i++){
+                if(t[i] == s[j])
+                    j++;
+    
+                if(j == s.size())
+                    return true;
+                
+            }
+    
+            return false;
+        }
+    };
